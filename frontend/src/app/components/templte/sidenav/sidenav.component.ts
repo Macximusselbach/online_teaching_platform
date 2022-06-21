07f418983ad2cb281f773abb473f6d4a938aa578
Macximus <macx.selbach@gmail.com>
2022-06-20 22:59:13 -0300

    terminando pagina login e verificação

--- frontend/src/app/components/templte/sidenav/sidenav.component.ts
+++ frontend/src/app/components/templte/sidenav/sidenav.component.ts
@@ -6,7 +6,7 @@ export interface Tile {
   color: string;
   cols: number;
   rows: number;
-  text: ;
+  text: string;
 }
 
 @Component({
--- frontend/src/app/views/login-view/login-view.component.ts
+++ frontend/src/app/views/login-view/login-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/student-service/studentService';
 import { student } from '../../models/student/student.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-view',
@@ -17,20 +18,24 @@ export class LoginViewComponent implements OnInit {
   //função principal de login
   async login(e: Event) {
 
+    e.preventDefault();
+
     const inputedCPF = (<HTMLInputElement>document.getElementById("cpf")).value;
     const inputedPassword = (<HTMLInputElement>document.getElementById("password")).value;
+    const labelCpf = (<HTMLInputElement>document.getElementById("label-cpf"));
 
     //busca estudante pelo cpf no banco de dados
     await this.loginService.searchStudentByCpf(inputedCPF).subscribe(student => {
 
-      if (student != null) {
-        this.studentLoging = student;
-        this.passwordValidator(inputedPassword);
-        console.log('achou cpf');
+      if (student.length != 1) {
+        labelCpf.innerText = 'CPF não encontrado!';
+        labelCpf.classList.add('wrong-login');
 
       } else {
-        e.preventDefault();
-        const labelCpf = (<HTMLInputElement>document.getElementById("label-cpf"));
+        labelCpf.innerText = 'CPF';
+        labelCpf.classList.remove('wrong-login');
+        this.studentLoging = student;
+        this.passwordValidator(inputedPassword);
 
       }
     });
@@ -41,20 +46,25 @@ export class LoginViewComponent implements OnInit {
 
   //função para validar a senha
   passwordValidator(password: string) {
-    console.log(this.studentLoging);
+    const labelPassword = (<HTMLInputElement>document.getElementById("label-password"));
+
+
     if (password == this.studentLoging[0].password) {
-      console.log('Entrando!');
+      labelPassword.innerText = 'Senha incorreta!';
+      labelPassword.classList.add('wrong-login');
 
       //armazena nome do estudante no cache do navegador / sessao
       window.sessionStorage.setItem('name', `${this.studentLoging[0].name}`);
+      this.router.navigate(['/home']);
 
     } else {
-      console.log('Senha incorreta!');
+      labelPassword.innerText = 'Senha incorreta!';
+      labelPassword.classList.add('wrong-login');
 
     }
   }
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
 
